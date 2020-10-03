a90f5a6897c672ebd5da7d736574e14fbd1eddeb
Sadika Chowdhury <sadikachowdhury29@gmail.com>
2020-10-02 21:17:32 -0400

    posting done

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ t1=blog()
 ### Viewing 
 
 
-@app.route('/posting')
+@app.route('/posting', methods=['POST'])
 def posting():
     t1.put(UserName='Sadi', UserID='123', BlogTitle="blog", BlogDescription="devbop")
     # if request.method == "POST":
@@ -18,15 +18,18 @@ def posting():
     #     Username = request.form['Username']
 
     #return postingID
-   
+    return "OK"
 
 @app.route('/comment', methods=["POST"])
 def comment():
     comment = request.form['Comment']
     blogID = request.form['blogID']
+    ## generate a commentID by using UUID
+    #commentID = UUID()
     #each blog content a unique ID
-    status= aws.saveComment(blogID, comment)
-    return  commentID
+    status= aws.saveComment(commentID, blogID, comment)
+    #return  commentID
+    return "OK"
 
 
 @app.route('/viewing', methods=["GET"])
