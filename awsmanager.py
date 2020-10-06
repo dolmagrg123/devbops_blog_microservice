0068941a0c1b6b9dba054cd51c953a45cc04f49f
CZ <dev.zombek@gmail.com>
2020-10-06 19:03:38 -0400

    added blog.py

--- awsmanager.py
+++ awsmanager.py
@@ -20,7 +20,7 @@ class blog:
             Item = {
                 self.Primary_Column_Name:last_primary_key,
                 self.columns[0]: UserName,
-                self.columns[1] : UserID,
+                self.columns[1] : Comment,
                 self.columns[2] : BlogTitle,
                 self.columns[3] : BlogDescription
                 
@@ -33,3 +33,31 @@ class blog:
         print(response["ResponseMetadata"]["HTTPStatusCode"])
 # t1=blog()
 # t1.put(UserName='Sadi', UserID='123', BlogTitle="blog", BlogDescription="devbop")
+
+
+
+def saveComment(self, comment):
+        self.userTable.put_item(Item=comment)
+def getContent(self, content):
+        res = self.userTable.get_item(
+            Key={'content': content}
+        )
+        if 'Item' in res:
+            return res['Item']
+        return None
+
+ def getByID(self, posting_id):
+        res = self.userTable.scan(
+            FilterExpression=Attr('id').eq(posting_id)
+        )
+        if res['Count'] == 0:
+            return None
+            return res['Items'][0]
+def getAllPost( self, posting_id, UserName)
+    res = self.userTable.get_item(
+            Key={'posting_id': posting_id}
+            key={'Username': Username}
+        )
+        if 'Item' in res:
+            return res['Item']
+        return None
