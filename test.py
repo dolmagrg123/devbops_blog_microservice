750086e9e13c3493841aa5101cac3e377d5dfe93
dolmagrg123 <62584166+dolmagrg123@users.noreply.github.com>
2021-02-03 10:55:04 -0500

    Update test.py

--- test.py
+++ test.py
@@ -35,93 +35,93 @@ class BasicTestCase(unittest.TestCase):
         data = json.loads(rv.data)
         assert data['Result'] == True
 
+    Result should be false
+    def test_3_DUPLICATE_posting(self):
+        req = {
+            "BlogName": "QATesting",
+            "BlogDate": date.today().strftime("%B %d, %Y"),
+            "BlogTime": datetime.now(),
+            "UserName": "QATest",
+            "BlogContent": "Test",
+            "BlogLocation": "location",
+            "BlogComment": {}
+        }
+
+        rv = self.app.post('/createBlog', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == False
+
+    def test_4_update(self):
+        req = {
+            "BlogName": "QATesting",
+            "New_BlogDate": date.today().strftime("%B %d, %Y"),
+            "New_BlogTime": datetime.now(),
+            "New_BlogContent": "blogBody",
+            "New_BlogLocation": "location"
+        }
+        rv = self.app.post('/update', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == True
+
+    def test_5_NONEXIST_udpate(self):
+        req = {
+            "BlogName": "_QATesting",
+            "New_BlogDate": date.today().strftime("%B %d, %Y"),
+            "New_BlogTime": datetime.now(),
+            "New_BlogContent": "blogBody",
+            "New_BlogLocation": "location"
+        }
+        rv = self.app.post('/update', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == False
+
+    def test_6_commenting(self):
+        req = {
+            "BlogName": "QATesting",
+            "UserName": "QA",
+            "Comment": "comment"
+        }
+        rv = self.app.post('/comment', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == True
+
+    def test_7_NONEXIST_commenting(self):
+        req = {
+            "BlogName": "_QATesting",
+            "UserName": "QA",
+            "Comment": "comment"
+        }
+        rv = self.app.post('/comment', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == False
+
+    def test_8_history(self):
+        req = {
+            "UserName": "QATest"
+        }
+        rv = self.app.post('/history', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == True
+
+
+    def test_9_deleting(self):
+        req = {
+            "BlogName": "QATesting"
+        }
+
+        rv = self.app.post('/delete', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == True
+
     # Result should be false
-#     def test_3_DUPLICATE_posting(self):
-#         req = {
-#             "BlogName": "QATesting",
-#             "BlogDate": date.today().strftime("%B %d, %Y"),
-#             "BlogTime": datetime.now(),
-#             "UserName": "QATest",
-#             "BlogContent": "Test",
-#             "BlogLocation": "location",
-#             "BlogComment": {}
-#         }
+    def test_10_NONEXIST_deleting(self):
+        req = {
+            "BlogName": "QATesting"
+        }
 
-#         rv = self.app.post('/createBlog', json=req)
-#         data = json.loads(rv.data)
-#         assert data['Result'] == False
-
-#     def test_4_update(self):
-#         req = {
-#             "BlogName": "QATesting",
-#             "New_BlogDate": date.today().strftime("%B %d, %Y"),
-#             "New_BlogTime": datetime.now(),
-#             "New_BlogContent": "blogBody",
-#             "New_BlogLocation": "location"
-#         }
-#         rv = self.app.post('/update', json=req)
-#         data = json.loads(rv.data)
-#         assert data['Result'] == True
-
-#     def test_5_NONEXIST_udpate(self):
-#         req = {
-#             "BlogName": "_QATesting",
-#             "New_BlogDate": date.today().strftime("%B %d, %Y"),
-#             "New_BlogTime": datetime.now(),
-#             "New_BlogContent": "blogBody",
-#             "New_BlogLocation": "location"
-#         }
-#         rv = self.app.post('/update', json=req)
-#         data = json.loads(rv.data)
-#         assert data['Result'] == False
-
-#     def test_6_commenting(self):
-#         req = {
-#             "BlogName": "QATesting",
-#             "UserName": "QA",
-#             "Comment": "comment"
-#         }
-#         rv = self.app.post('/comment', json=req)
-#         data = json.loads(rv.data)
-#         assert data['Result'] == True
-
-#     def test_7_NONEXIST_commenting(self):
-#         req = {
-#             "BlogName": "_QATesting",
-#             "UserName": "QA",
-#             "Comment": "comment"
-#         }
-#         rv = self.app.post('/comment', json=req)
-#         data = json.loads(rv.data)
-#         assert data['Result'] == False
-
-#     def test_8_history(self):
-#         req = {
-#             "UserName": "QATest"
-#         }
-#         rv = self.app.post('/history', json=req)
-#         data = json.loads(rv.data)
-#         assert data['Result'] == True
-
-
-#     def test_9_deleting(self):
-#         req = {
-#             "BlogName": "QATesting"
-#         }
-
-#         rv = self.app.post('/delete', json=req)
-#         data = json.loads(rv.data)
-#         assert data['Result'] == True
-
-#     # Result should be false
-#     def test_10_NONEXIST_deleting(self):
-#         req = {
-#             "BlogName": "QATesting"
-#         }
-
-#         rv = self.app.post('/delete', json=req)
-#         data = json.loads(rv.data)
-#         assert data['Result'] == False
+        rv = self.app.post('/delete', json=req)
+        data = json.loads(rv.data)
+        assert data['Result'] == False
 
 
 
